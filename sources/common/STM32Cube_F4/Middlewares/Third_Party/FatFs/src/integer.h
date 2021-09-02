d2449bf9054491efcff04f34a9e8c2dbf663587c
Sasha7b9 <Sasha7b9@tut.by>
2021-09-02 08:56:06 +0300

    Добавлены файлы Cube

---
+++ sources/common/STM32Cube_F4/Middlewares/Third_Party/FatFs/src/integer.h
@@ -0,0 +1,27 @@
+/*-------------------------------------------*/
+/* Integer type definitions for FatFs module */
+/*-------------------------------------------*/
+
+#ifndef _FF_INTEGER
+#define _FF_INTEGER
+
+/* These types MUST be 16-bit or 32-bit */
+typedef int				INT; //-V677
+typedef unsigned int	UINT; //-V677
+
+/* This type MUST be 8-bit */
+typedef unsigned char	BYTE; //-V677
+
+/* These types MUST be 16-bit */
+typedef short			SHORT; //-V677
+typedef unsigned short	WORD; //-V677
+typedef unsigned short	WCHAR; //-V677
+
+/* These types MUST be 32-bit */
+typedef long			LONG; //-V677
+typedef unsigned long	DWORD; //-V677
+
+/* This type MUST be 64-bit (Remove this for ANSI C (C89) compatibility) */
+typedef unsigned long long QWORD; //-V677
+
+#endif
