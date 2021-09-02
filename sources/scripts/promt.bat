90e02c4f536468ef043eae8988e78fc0bdc07e76
Sasha7b9 <Sasha7b9@tut.by>
2021-09-02 08:26:08 +0300

    Добавлены исходники

---
+++ sources/scripts/promt.bat
@@ -0,0 +1,5 @@
+@echo off
+SET COMPILER=c:\Keil_v5\UV4\UV4
+set PATH=%PATH%;C:\Program Files\CMake\bin;c:\Program Files\Git\mingw64\bin\;C:\Users\Sasha7b9\AppData\Local\Atlassian\SourceTree\git_local\cmd\
+%comspec% /k "c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+@echo on
