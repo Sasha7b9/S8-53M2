ca9d986da129355000f40df2d89bf0bf8d71d29c
Sasha7b9 <Sasha7b9@tut.by>
2022-02-11 17:55:40 +0300

    *

--- sources/Device/src/Display/Display.cpp
+++ sources/Device/src/Display/Display.cpp
@@ -417,11 +417,11 @@ void Display::DrawMath()
 
     DrawDataChannel(points, Math, ds, Grid::MathTop(), Grid::MathBottom());
 
-    static const int WIDTH = 71;
-    static const int HEIGHT = 10;
+    static const int width = 71;
+    static const int height = 10;
     int delta = (SHOW_STRING_NAVIGATION && MODE_DRAW_MATH_IS_TOGETHER) ? 10 : 0;
-    Painter::DrawRectangleC(Grid::Left(), Grid::MathTop() + delta, WIDTH, HEIGHT, COLOR_FILL);
-    Painter::FillRegionC(Grid::Left() + 1, Grid::MathTop() + 1 + delta, WIDTH - 2, HEIGHT - 2, COLOR_BACK);
+    Painter::DrawRectangleC(Grid::Left(), Grid::MathTop() + delta, width, height, COLOR_FILL);
+    Painter::FillRegionC(Grid::Left() + 1, Grid::MathTop() + 1 + delta, width - 2, height - 2, COLOR_BACK);
     Divider multiplier = MATH_MULTIPLIER;
     Painter::DrawTextC(Grid::Left() + 2, Grid::MathTop() + 1 + delta, sChannel_Range2String(SET_RANGE_MATH, multiplier), COLOR_FILL);
     Painter::DrawText(Grid::Left() + 25, Grid::MathTop() + 1 + delta, ":");
