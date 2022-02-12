b5feff45c916bf08e42be76a68f6312603e64981
Sasha7b9 <Sasha7b9@tut.by>
2022-02-12 21:57:11 +0300

    *

--- sources/Device/src/Device.cpp
+++ sources/Device/src/Device.cpp
@@ -19,54 +19,7 @@
 
 namespace Device
 {
-    static void ProcessingSignal()
-    {
-        uint8 **data0 = &gData0;
-        uint8 **data1 = &gData1;
-        DataSettings **ds = &gDSet;
-
-        int first = 0;
-        int last = 0;
-        sDisplay_PointsOnDisplay(&first, &last);
-
-        if (MODE_WORK_IS_DIRECT)
-        {
-            Storage::GetDataFromEnd(0, &gDSet, &gData0, &gData1);
-            if (sDisplay_NumAverage() != 1 || sTime_RandomizeModeEnabled())
-            {
-                gData0 = Storage::GetAverageData(Chan::A);
-                gData1 = Storage::GetAverageData(Chan::B);
-            }
-        }
-        else if (MODE_WORK_IS_LATEST)
-        {
-            data0 = &gData0memLast;
-            data1 = &gData1memLast;
-            ds = &gDSmemLast;
-            Storage::GetDataFromEnd(CURRENT_NUM_LATEST_SIGNAL, &gDSmemLast, &gData0memLast, &gData1memLast);
-        }
-        else if (MODE_WORK_IS_MEMINT)
-        {
-            data0 = &gData0memInt;
-            data1 = &gData1memInt;
-            ds = &gDSmemInt;
-            EPROM::GetData(CURRENT_NUM_INT_SIGNAL, &gDSmemInt, &gData0memInt, &gData1memInt);
-        }
-
-        if (MODE_WORK_IS_MEMINT)
-        {
-            if (!MODE_SHOW_MEMINT_IS_SAVED)
-            {
-                Processing::SetSignal(gData0, gData1, gDSet, first, last);
-            }
-        }
-        else
-        {
-            Processing::SetSignal(*data0, *data1, *ds, first, last);
-        }
-
-        Cursors_Update();    // В случае, если находимся в режиме курсорных измерений, обновляем их положение, если нужно.
-    }
+    static void ProcessingSignal();
 }
 
 
@@ -105,4 +58,55 @@ void Device::Update()
 
     // LAN::Update(0);
     // VCP::Update();
+}
+
+
+static void Device::ProcessingSignal()
+{
+    uint8** data0 = &gData0;
+    uint8** data1 = &gData1;
+    DataSettings** ds = &gDSet;
+
+    int first = 0;
+    int last = 0;
+    sDisplay_PointsOnDisplay(&first, &last);
+
+    if (MODE_WORK_IS_DIRECT)
+    {
+        Storage::GetDataFromEnd(0, &gDSet, &gData0, &gData1);
+        if (sDisplay_NumAverage() != 1 || sTime_RandomizeModeEnabled())
+        {
+            gData0 = Storage::GetAverageData(Chan::A);
+            gData1 = Storage::GetAverageData(Chan::B);
+        }
+    }
+    else if (MODE_WORK_IS_LATEST)
+    {
+        data0 = &gData0memLast;
+        data1 = &gData1memLast;
+        ds = &gDSmemLast;
+        Storage::GetDataFromEnd(CURRENT_NUM_LATEST_SIGNAL, &gDSmemLast, &gData0memLast, &gData1memLast);
+    }
+    else if (MODE_WORK_IS_MEMINT)
+    {
+        data0 = &gData0memInt;
+        data1 = &gData1memInt;
+        ds = &gDSmemInt;
+        EPROM::GetData(CURRENT_NUM_INT_SIGNAL, &gDSmemInt, &gData0memInt, &gData1memInt);
+    }
+
+    if (MODE_WORK_IS_MEMINT)
+    {
+        if (!MODE_SHOW_MEMINT_IS_SAVED)
+        {
+            Processing::SetSignal(gData0, gData1, gDSet, first, last);
+        }
+    }
+    else
+    {
+        Processing::SetSignal(*data0, *data1, *ds, first, last);
+    }
+
+    Cursors_Update();    // В случае, если находимся в режиме курсорных измерений, обновляем их положение, если нужно.
+
 }
