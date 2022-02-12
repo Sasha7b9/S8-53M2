b85ae1e99c928d1ccda6792e7a96da4ce49d4d5e
Sasha7b9 <Sasha7b9@tut.by>
2022-02-12 12:42:39 +0300

    *

--- sources/common/_VS_GUI/CMSIS/core_cm4.h
+++ sources/common/_VS_GUI/CMSIS/core_cm4.h
@@ -5,3 +5,16 @@
 #define __disable_irq()
 #define __set_MSP(x)
 #define __enable_irq();
+
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+
+#define __IO volatile
+#define __STATIC_INLINE inline
+#define __CLZ
+
+inline uint32_t __RBIT(uint32_t value)
+{
+    return value;
+}
+    
