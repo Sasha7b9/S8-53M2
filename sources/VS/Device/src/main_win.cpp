9444b3b71bf895f75509b069b51a038bf626190d
Sasha7b9 <Sasha7b9@tut.by>
2022-02-12 12:44:49 +0300

    *

--- sources/VS/Device/src/main_win.cpp
+++ sources/VS/Device/src/main_win.cpp
@@ -1,8 +1,5 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "common/Display/Painter/Text_.h"
-#include "common/Hardware/Memory/Sector_.h"
-#include "device.h"
 #include "GUI/Application.h"
 #include "Menu/Menu.h"
 #include "Settings/Settings.h"
