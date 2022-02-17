9574325ee71288a153bb2f581aaac77d3ceb89f7
Sasha7b9 <Sasha7b9@tut.by>
2022-02-17 16:20:02 +0300

    *

--- sources/Device/src/Settings/SettingsTime.cpp
+++
@@ -1,6 +0,0 @@
-// 2022/2/11 19:49:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
-#include "SettingsTime.h"
-#include "Settings.h"
-#include "Log.h"
-#include "Utils/Math.h"
-#include "FPGA/FPGA.h"
