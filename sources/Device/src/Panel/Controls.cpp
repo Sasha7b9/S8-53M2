bc6f89637d1bbb72215f99f9a7aade08a66ccdd2
Sasha7b9 <Sasha7b9@tut.by>
2022-02-18 11:08:05 +0300

    *

--- sources/Device/src/Panel/Controls.cpp
+++ sources/Device/src/Panel/Controls.cpp
@@ -3,33 +3,31 @@
 #include "Panel/Controls.h"
 
 
-static pchar  namesButtons[] = 
-{
-    "B_Empty",
-    "B_ChannelA",
-    "B_Service",
-    "B_ChannelB",
-    "B_Display",
-    "B_Time",
-    "B_Memory",
-    "B_Sinchro",
-    "B_Start",
-    "B_Cursors",
-    "B_Measures",
-    "B_Power",
-    "B_Info",
-    "B_Menu",
-    "B_F1",
-    "B_F2",
-    "B_F3",
-    "B_F4",
-    "B_F5"
-};
-
-
-
 pchar  NameButton(PanelButton button) 
 {
+    static const pchar namesButtons[] =
+    {
+        "B_Empty",
+        "B_ChannelA",
+        "B_Service",
+        "B_ChannelB",
+        "B_Display",
+        "B_Time",
+        "B_Memory",
+        "B_Sinchro",
+        "B_Start",
+        "B_Cursors",
+        "B_Measures",
+        "B_Power",
+        "B_Info",
+        "B_Menu",
+        "B_F1",
+        "B_F2",
+        "B_F3",
+        "B_F4",
+        "B_F5"
+    };
+
     return namesButtons[button];
 }
 
