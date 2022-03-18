dac306e9fefa2370d4aab4032f8f2248f4e290f2
Sasha7b9 <Sasha7b9@tut.by>
2022-03-18 14:12:00 +0300

    Подготовлены функции для открытия и закрытия фреймов

--- sources/Device/src/Data/Storage.cpp
+++ sources/Device/src/Data/Storage.cpp
@@ -113,6 +113,27 @@ void Storage::AddData(DataStruct &data)
 }
 
 
+void Storage::OpenFrame()
+{
+    DataSettings ds;
+    ds.Init();
+
+    PrepareNewFrame(ds);
+}
+
+
+void Storage::CloseFrame()
+{
+    DataSettings *ds = GetDataSettings(0);
+
+    ds->time = HAL_RTC::GetPackedTime();
+
+    CalculateLimits(ds, ds->DataBegin(ChA), ds->DataEnd(ChB));
+
+    Averager::Append(ds);
+}
+
+
 int Storage::NumFrames()
 {
     return count_data;
