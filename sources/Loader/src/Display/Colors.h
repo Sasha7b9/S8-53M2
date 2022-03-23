bef89228337210c32f47fa9e594fd5b1c0d0afd0
Sasha7b9 <Sasha7b9@tut.by>
2022-03-23 09:28:44 +0300

    detash

--- sources/Loader/src/Display/Colors.h
+++ sources/Loader/src/Display/Colors.h
@@ -5,32 +5,6 @@
 #include "DisplayTypes.h"
 
 
-// Возвращает цвет синхронизации.
-Color::E ColorTrig();
-// Цвет заголовка страницы, inShade - затенена ли страница.
-Color::E ColorMenuTitle(bool inShade);
-// Цвет элемента меню.
-Color::E ColorMenuItem(bool inShade);
-// Цвет окантовки меню.
-Color::E ColorBorderMenu(bool inShade);
-// Возвращает цвет, контрастный к color. Может быть белым или чёрным.
-Color::E ColorContrast(Color::E color);
-// Светлый цвет в тени.
-Color::E LightShadingTextColor();
-
-
-#define ColorMenuField()            Color::MENU_FIELD
-// Чуть менее светлый цвет, чем цвет элемента меню. Используется для создания эффекта объёма.
-#define ColorMenuItemLessBright()   Color::MENU_ITEM_DARK
-// Чуть более светлый цвет, чем цвет заголовка страницы. Используется для создания эффекта объёма.
-#define ColorMenuTitleBrighter()    Color::MENU_TITLE_BRIGHT
-// Чуть менее светлый цвет, чем цвет заголовка страницы. Используется для создания эффекта объёма.
-#define ColorMenuTitleLessBright()  Color::MENU_TITLE_DARK
-// Чуть более светлый цвет, чем цвет элемента меню. Используется для создания эффекта объёма.
-#define ColorMenuItemBrighter()     Color::MENU_ITEM_BRIGHT
-// Тёмный цвет в тени.
-#define DarkShadingTextColor()      Color::MENU_TITLE_DARK
-
 
 #define MAKE_COLOR(r, g, b) (((b) & 0x1f) + (((g) & 0x3f) << 5) + (((r) & 0x1f) << 11))
 #define R_FROM_COLOR(color) (((uint16)(color) >> 11) & (uint16)0x1f)
@@ -39,5 +13,3 @@ Color::E LightShadingTextColor();
 
 extern Color::E gColorFill;
 extern Color::E gColorBack;
-extern Color::E gColorGrid;
-extern Color::E gColorChan[4];
