6e3efc86ba5831e2702662190ee33d8ed5931a77
Sasha7b9 <Sasha7b9@tut.by>
2022-03-29 21:14:02 +0300

    detrash

--- sources/Device/src/SCPI/SCPI.cpp
+++ sources/Device/src/SCPI/SCPI.cpp
@@ -28,13 +28,6 @@ static int pointer = 0;
 
 void SCPI::AddNewData(uint8 *data, uint length)
 {
-//    BufferHeap<char> message;
-//    message.ReallocFromBuffer((char *)data, (int)length + 1);
-//    message.Data()[message.Size() - 2] = '\0';
-//    LOG_WRITE(message.DataChar());
-//
-//    return;
-
     std::memcpy(&bufData[pointer], data, length);
     pointer += length;
 
