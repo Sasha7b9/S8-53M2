624ae69aa7fe8f06b2485111055f9180b89c37ee
Sasha7b9 <Sasha7b9@tut.by>
2022-04-07 21:32:26 +0300

    *

--- sources/Device/src/Display/DisplayTypes.h
+++ sources/Device/src/Display/DisplayTypes.h
@@ -57,8 +57,6 @@ enum Command
     DRAW_TEXT                 = 0x08,   //  *
     SET_PALETTE               = 0x09,   //  *  9 numColor:1 colorValue:2
     SET_FONT                  = 0x0a,   //  *  10 numFont:1
-    DRAW_HPOINT_LINE          = 0x0b,   //     
-    DRAW_VPOINT_LINE          = 0x0c,   //     12 x:2 y0:1 delta:1 numPoints:1
     DRAW_VLINES_ARRAY         = 0x0d,   //  *  13 x0:2 numLines:1 lines:numLines * 2
     DRAW_SIGNAL_POINTS        = 0x0e,   //  *  14 x:2 points:281
     DRAW_MULTI_HPOINT_LINES   = 0x11,   //  *  17 numLines:1 x:2 numPoints:1 dx:1 lines:numLines
