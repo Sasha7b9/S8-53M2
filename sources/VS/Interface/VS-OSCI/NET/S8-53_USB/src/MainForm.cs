781d1a46f20a2519a2afa4e14b66541651a3d6dd
Sasha7b9 <Sasha7b9@tut.by>
2022-04-07 21:13:22 +0300

    *

--- sources/VS/Interface/VS-OSCI/NET/S8-53_USB/src/MainForm.cs
+++ sources/VS/Interface/VS-OSCI/NET/S8-53_USB/src/MainForm.cs
@@ -46,21 +46,21 @@ namespace S8_53_USB {
 
         private enum Command : byte
         {
-            SET_COLOR = 1,
-            FILL_REGION = 2,
-            END_SCENE = 3,
-            DRAW_HLINE = 4,
-            DRAW_VLINE = 5,
-            SET_POINT = 6,
-            DRAW_SIGNAL_LINES = 7,
-            DRAW_TEXT = 8,
-            SET_PALETTE = 9,
-            SET_FONT = 10,
-            DRAW_VLINES_ARRAY = 13,
-            DRAW_SIGNAL_POINTS = 14,
+            SET_COLOR               = 1,
+            FILL_REGION             = 2,
+            END_SCENE               = 3,
+            DRAW_HLINE              = 4,
+            DRAW_VLINE              = 5,
+            SET_POINT               = 6,
+            DRAW_SIGNAL_LINES       = 7,
+            DRAW_TEXT               = 8,
+            SET_PALETTE             = 9,
+            SET_FONT                = 10,
+            DRAW_VLINES_ARRAY       = 13,
+            DRAW_SIGNAL_POINTS      = 14,
             DRAW_MULTI_HPOINT_LINES = 17,
             DRAW_MULTI_VPOINT_LINES = 18,
-            LOAD_FONT = 19
+            LOAD_FONT               = 19
         };
 
         public MainForm() {
