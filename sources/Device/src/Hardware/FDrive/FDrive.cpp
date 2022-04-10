f4d6500bad0649282cdd4b8b8254061339ce28a3
Sasha7b9 <Sasha7b9@tut.by>
2022-04-10 18:39:30 +0300

    refact

--- sources/Device/src/Hardware/FDrive/FDrive.cpp
+++ sources/Device/src/Hardware/FDrive/FDrive.cpp
@@ -11,7 +11,8 @@
 #include "ffconf.h"
 #include "FDrive.h"
 #include "Hardware/HAL/HAL.h"
-
+#include "Display/Painter.h"
+#include "Utils/Text/Warnings.h"
 
 
 static FATFS USBDISKFatFs;
@@ -27,6 +28,16 @@ namespace FDrive
     bool needSave = false;
 
     void ChangeState();
+
+    void SaveAsBMP();
+
+    void SaveAsText();
+}
+
+
+namespace Painter
+{
+    Color::E GetColor(int x, int y);
 }
 
 
@@ -467,4 +478,143 @@ pchar FileName::Extract()
     } while (pointer != c_str());
 
     return pointer;
+}
+
+
+void FDrive::SaveSignal()
+{
+    if (!needSave)
+    {
+        return;
+    }
+
+    needSave = false;
+
+    if (MODE_SAVE_SIGNAL_IS_BMP)
+    {
+        SaveAsBMP();
+    }
+    else
+    {
+        SaveAsText();
+    }
+}
+
+
+void FDrive::SaveAsBMP()
+{
+#pragma pack(1)
+    struct BITMAPFILEHEADER
+    {
+        char    type0;      // 0
+        char    type1;      // 1
+        uint    size;       // 2
+        uint16  res1;       // 6
+        uint16  res2;       // 8
+        uint    offBits;    // 10
+    }
+    bmFH =
+    {
+        0x42,
+        0x4d,
+        14 + 40 + 1024 + 320 * 240,
+        0,
+        0,
+        14 + 40 + 1024
+    };
+    // 14
+
+    struct BITMAPINFOHEADER
+    {
+        uint    size;           // 14
+        int     width;          // 18
+        int     height;         // 22
+        uint16  planes;         // 26
+        uint16  bitCount;       // 28
+        uint    compression;    // 30
+        uint    sizeImage;      // 34
+        int     xPelsPerMeter;  // 38
+        int     yPelsPerMeter;  // 42
+        uint    clrUsed;        // 46
+        uint    clrImportant;   // 50
+        //uint    notUsed[15];
+    }
+    bmIH =
+    {
+        40, // size;
+        320,// width;
+        240,// height;
+        1,  // planes;
+        8,  // bitCount;
+        0,  // compression;
+        0,  // sizeImage;
+        0,  // xPelsPerMeter;
+        0,  // yPelsPerMeter;
+        0,  // clrUsed;
+        0   // clrImportant;
+    };
+    // 54
+#pragma pack(4)
+
+    File file;
+
+    String<> fileName = FM::GetNameForNewFile();
+
+    if (!fileName.Size())
+    {
+        LOG_ERROR("Не получено имя для файла");
+        return;
+    }
+
+    if (file.OpenNewForWrite(fileName.c_str()))
+    {
+        file.Write((uint8 *)(&bmFH), 14);
+
+        file.Write((uint8 *)(&bmIH), 40);
+
+        uint8 buffer[320 * 3] = {0};
+
+        struct RGBQUAD
+        {
+            uint8    blue;
+            uint8    green;
+            uint8    red;
+            uint8    rgbReserved;
+        };
+
+        RGBQUAD colorStruct;
+
+        for (int i = 0; i < 16; i++)
+        {
+            uint color = COLOR(i);
+            colorStruct.blue = B_FROM_COLOR(color);
+            colorStruct.green = G_FROM_COLOR(color);
+            colorStruct.red = R_FROM_COLOR(color);
+            colorStruct.rgbReserved = 0;
+            ((RGBQUAD *)(buffer))[i] = colorStruct;
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            file.Write(buffer, 256);
+        }
+
+        for (int y = 239; y >= 0; y--)
+        {
+            for (int x = 1; x < 320; x++)
+            {
+                buffer[x] = (uint8)Painter::GetColor(x, y);
+            }
+
+            file.Write(buffer, 320);
+        }
+
+        Warning::ShowGood(Warning::FileIsSaved);
+    }
+}
+
+
+void FDrive::SaveAsText()
+{
+
 }
